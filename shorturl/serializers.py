f4c1e251f19a338f6fdb798b146a5f58e3883f26
Arnaud Clauss <arnaud.clauss@student.ecp.fr>
2017-06-10 17:49:22 +0200

    Add serializers and views to list, create and retrieve user and shortcut

--- shorturl/serializers.py
+++ shorturl/serializers.py
@@ -1,15 +1,39 @@
 from rest_framework import serializers
-from shortUrl.models import ShortcutURL
+from shorturl.models import ShortcutURL
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+import string
+import random
 
 class ShortcutUrlSerializer(serializers.ModelSerializer):
-	owner = serializers.ReadOnlyField(source='owner.username')
+    owner = serializers.ReadOnlyField(source='owner.username')
+    shortcut = serializers.CharField(max_length=7, required=False, validators=[
+        RegexValidator(regex=r'^\w{7}$', message='Shortcut must contain 7 digits or letters')])
+    numberClick = serializers.ReadOnlyField()
+
+    def create(self, validated_data):
+        if not 'shortcut' in validated_data:
+            shortcut = ''.join(random.choices(string.ascii_letters+string.digits, k=7))
+            while ShortcutURL.objects.filter(shortcut=shortcut):
+                shortcut = ''.join(random.choices(string.ascii_letters, k=7))
+            validated_data['shortcut'] = shortcut
+        return ShortcutURL(**validated_data)
+
     class Meta:
         model = ShortcutURL
         fields = ('shortcut', 'url', 'title', 'owner', 'numberClick')
 
+
 class UserSerializer(serializers.ModelSerializer):
-    urls = serializers.PrimaryKeyRelatedField(many=True, queryset=ShortcutURL.objects.all())
+    urls = serializers.HyperlinkedRelatedField(many=True, view_name='shortcuturl-detail', read_only=True)
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create(username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'urls')
+        fields = ('id', 'username', 'urls', 'password')
--- shorturl/views.py
+++ shorturl/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from shorturl.models import ShortcutURL
+from shorturl.serializers import ShortcutUrlSerializer, UserSerializer
 
-# Create your views here.
+# This ViewSet provides 'list' and 'detail' action
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    #TODO Add permission
+
+class ShortcutUrlsViewSet(viewsets.ModelViewSet):
+    queryset = ShortcutURL.objects.all()
+    serializer_class = ShortcutUrlSerializer
+    #TODO Add permission
+
+    def perform_create(self, serializer):
+        if self.request.user.is_authenticated:
+            serializer.save(owner=self.request.user, numberClick=0)
+        else:
+            serializer.save()

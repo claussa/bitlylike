0b278baafb7091ba1b8907d7791da02c5aa11b05
Arnaud Clauss <arnaud.clauss@student.ecp.fr>
2017-06-10 16:00:11 +0200

    Add database model

--- shorturl/models.py
+++ shorturl/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Model to store URL shortcut
+class ShortcutURL(models.Model):
+    shortcut = models.CharField(max_length=7, primary_key=True)
+    title = models.CharField(max_length=40, blank=True)
+    url = models.URLField()
+    owner = models.ForeignKey(User, related_name='urls', on_delete=models.CASCADE, blank=True)
+    numberClick = models.IntegerField(blank=True)

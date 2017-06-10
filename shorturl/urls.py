3f674773b97d1507f96eb0179ea7402974c25d70
Arnaud Clauss <arnaud.clauss@student.ecp.fr>
2017-06-10 18:10:16 +0200

    Add redirection and count the number of redirection

--- shorturl/urls.py
+++ shorturl/urls.py
@@ -2,11 +2,15 @@ from django.conf.urls import url, include
 from shorturl import views
 from rest_framework.routers import DefaultRouter
 
+# Route for the REST API
 router = DefaultRouter()
 router.register(r'shorturls', views.ShortcutUrlsViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+# include the REST API's routes
     url(r'^api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls'))
+    url(r'^api-auth/', include('rest_framework.urls')),
+# Redirection route
+	url(r'^(?P<shortcut>\w{7})', views.redirection)
 ]
--- shorturl/views.py
+++ shorturl/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
 from rest_framework import viewsets
 from django.contrib.auth.models import User
 from shorturl.models import ShortcutURL
 from shorturl.serializers import ShortcutUrlSerializer, UserSerializer
+from django.core.exceptions import ObjectDoesNotExist
 
 # This ViewSet provides 'list' and 'detail' action
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +22,15 @@ class ShortcutUrlsViewSet(viewsets.ModelViewSet):
             serializer.save(owner=self.request.user, numberClick=0)
         else:
             serializer.save()
+
+def redirection(request, **arguments):
+    try:
+        shortcut = ShortcutURL.objects.get(shortcut=arguments['shortcut'])
+    except ObjectDoesNotExist:
+        return Http404("Redirection does not exist")
+    else:
+        print(shortcut.numberClick)
+        if shortcut.numberClick is not None:
+            shortcut.numberClick += 1
+            shortcut.save()
+        return redirect(shortcut.url)
